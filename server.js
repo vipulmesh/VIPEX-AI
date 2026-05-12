b82a78690c56b090994a1497d42b08d1fd6951e3
Vipul Meshram <vipulmeshramv20@gmail.com>
2026-05-12 13:34:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -50,9 +50,8 @@ app.post("/chat", async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
 
-    console.log(
-        "Server running on http://localhost:3000"
-    );
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
